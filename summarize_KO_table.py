41314edb2d99c6fba5d24637ed20e90f93281870
Jon <jonathan.leff@colorado.edu>
2013-12-05 19:49:12 -0700

    hack code to get things started

---
+++ summarize_KO_table.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+
+import sys
+import os
+
+
+"""
+Script to build a summary table at a specified KO hierarchy level given
+a gene abundance table and a KO/corresponding hierarchy dictionary. This
+script takes into account multiple hierarchies for a single KO number
+by counting the abundance of the gene in all categories to which it
+belongs.
+"""
+
+geneTable_fp = '/Users/leffj/Dropbox/genomes_traits_project/fresh_start/merged_L4_500000_noHierarc_6spls.txt'
+output_dir = '/Users/leffj/Dropbox/genomes_traits_project/fresh_start/summaries'
+koDictionary_fp = '/Users/leffj/Dropbox/genomes_traits_project/fresh_start/KO_dictionary_img/gene_ko_dictionary.txt'
+
+
+
+def import_dictionary(dictionary_fp):
+	rawDict = open(koDictionary_fp,'U')
+	outDict = {}
+	for line in rawDict:
+		key = line.split('\t')[0]
+		entry = '\t'.join(line.strip().split('\t')[1:])
+		outDict[key] = entry
+	rawDict.close()
+	return outDict
+
+def get_cats_from_hierarchies(geneTable,koDictionary,level):
+	# get sampleIDs
+	sampleIDs = geneTable.readline().strip().split('\t')[1:]
+	# loop through each KO
+	summaryCatsAbunds = {}
+	for line in geneTable:
+		splitline = line.strip().split('\t')
+		ko = splitline[0]
+		abunds = splitline[1:]
+		abunds = [float(x) for x in abunds]
+		# skip kos with abundance 0 for every sample
+		if sum(abunds) == 0:
+			continue
+		try:
+			koHiers = koDictionary[ko].split('\t')
+		except KeyError:
+			print 'No match in dictionary for:%s' %(ko)
+		# loop through hierarchies for ko and pull cats at desired level
+		# only pull the same cat once to avoid double counting
+		cats = []
+		for hier in koHiers:
+			hierAsList = hier.strip().split(';')
+			if not hierAsList[level-1].strip() in cats:
+				cats.append(hierAsList[level-1].strip())
+		for cat in cats:
+			if not cat in summaryCatsAbunds:
+				summaryCatsAbunds[cat] = abunds
+			else:
+				summaryCatsAbunds[cat] = [(x + y) for x, y in \
+				 zip(summaryCatsAbunds[cat], abunds)]
+	return sampleIDs,summaryCatsAbunds
+
+def get_relative_abundances(summary):
+	sums = []
+	for key in summary:
+		if sums == []:
+			sums = summary[key]
+		else:
+			sums = [(x + y) for x, y in zip(sums,summary[key])]
+	relAbunds = summary
+	for key in summary:
+		relAbunds[key] = [(x / y) for x, y in zip(summary[key], sums)]
+	return relAbunds
+
+def main():
+	# import KO dictionary
+	koDictionary = import_dictionary(koDictionary_fp)
+
+	# create output directory if doesn't exist and create
+	if not os.path.exists(output_dir):
+		os.makedirs(output_dir)
+
+	# get output filepath prefix
+	input_file_basename, input_file_ext = \
+	 os.path.splitext(os.path.split(geneTable_fp)[1])
+
+	# loop through each KO, get category for each hierarchy to which the
+	# KO belongs/given level, abundances, and return summary
+	levels = [1,2,3]
+	for level in levels:
+		sampleIDs,summary = \
+		 get_cats_from_hierarchies(open(geneTable_fp,'U'),koDictionary,level)
+
+		# get relative abundances
+		summaryRAs = get_relative_abundances(summary)
+
+		out_fp = '%s/%s_L%s.txt' %(output_dir,input_file_basename,level)
+		out = open(out_fp,'w')
+
+		out.write('Category\t' + '\t'.join(sampleIDs) + '\n')
+		for key in summaryRAs:
+			RAs = [str(x) for x in summaryRAs[key]]
+			outLine = '%s\t%s\n' %(key,'\t'.join(RAs))
+			out.write(outLine)
+		out.close()
+
+
+if __name__ == "__main__":
+	main()
